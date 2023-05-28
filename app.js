a60851d1a059a6a3457ce8296650f5a47bbcb1ec
Joshua Gregory <yoshig5114@gmail.com>
2023-05-28 08:58:16 -0400

    exercise 2

--- app.js
+++ app.js
@@ -1,4 +1,42 @@
-console.log("Hello World!\n==========\n");
+
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+function plus(num){
+    return (plusNum) => {
+        return num + plusNum;
+    }
+}
+let plus15 = plus(15);
+console.log(plus15(10));
+
+//exercise 2
+console.log("EXERCISE 1:\n==========\n");
+
+let users = [
+    {
+      name: "Frodo",
+      age: 50,
+      score: 85,
+      isActive: false,
+    },
+    {
+      name: "Sam",
+      age: 38,
+      score: 94,
+      isActive: true,
+    },
+    {
+      name: "Merry",
+      age: 36,
+      score: 82,
+      isActive: true,
+    },
+    {
+      name: "Pippin",
+      age: 26,
+      score: 77,
+      isActive: false,
+    },
+  ];
+  
